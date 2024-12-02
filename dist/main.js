b087a1761ac53cbdc1418f2e95d15d834574c277
GUTS <140621928+G-U-T-S@users.noreply.github.com>
2024-12-02 14:42:50 +0000

    new file:   assets/belt.png 	new file:   assets/spritesheets/belt.json 	modified:   dist/main.js 	modified:   src/main.ts

--- dist/main.js
+++ dist/main.js
@@ -1,7 +1,7 @@
 import * as PIXI from 'https://cdn.jsdelivr.net/npm/pixi.js@8.5.2/dist/pixi.min.mjs';
 import { Grid } from './grid.js';
 import { PlacebleObject } from './placeableObject.js';
-import { initDevtools } from '../node_modules/@pixi/devtools/dist/index.js';
+import { initDevtools } from '../node_modules/@pixi/devtools/dist/index.js'; //* DEBUG
 class ItemSource extends PlacebleObject {
     constructor(position = { x: 0, y: 0 }) {
         super({ x: 0, y: 0 }, { ...position });
@@ -16,11 +16,16 @@ class ItemSource extends PlacebleObject {
     app.stage.eventMode = "static";
     app.stage.hitArea = app.screen;
     document.body.appendChild(app.canvas);
+    const canvas = document.querySelector("canvas");
+    //& desativa o menu do botão direito
+    app.canvas.addEventListener("contextmenu", (event) => {
+        event.preventDefault();
+    });
     let index = 0;
     const bgDark = await PIXI.Assets.load("../assets/bgDark.png");
     const bgLight = await PIXI.Assets.load("../assets/bgLight.png");
     const BG = new PIXI.Container();
-    //! inefficient, just a debug;
+    //! inefficient;
     for (let Y = 0; Y < 22; Y++) {
         for (let X = 0; X < 39; X++) {
             const sprite = new PIXI.Sprite();
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import * as PIXI from 'https://cdn.jsdelivr.net/npm/pixi.js@8.5.2/dist/pixi.min.mjs';
 import { Grid } from './grid.js';
 import { PlacebleObject } from './placeableObject.js';
-import { initDevtools } from '../node_modules/@pixi/devtools/dist/index.js';
+import { initDevtools } from '../node_modules/@pixi/devtools/dist/index.js';//* DEBUG
 
 
 class ItemSource extends PlacebleObject {
@@ -13,7 +13,6 @@ class ItemSource extends PlacebleObject {
 }
 
 
-
 (async () => {
   const app = new PIXI.Application();
   await app.init({
@@ -22,14 +21,19 @@ class ItemSource extends PlacebleObject {
   });
   app.stage.eventMode = "static";
   app.stage.hitArea = app.screen;
-  document.body.appendChild(app.canvas);
-
+  document.body.appendChild(app.canvas);  const canvas = document.querySelector("canvas");
+  
+  //& desativa o menu do botão direito
+  app.canvas.addEventListener("contextmenu", (event) => {
+      event.preventDefault();
+  });
+  
   let index: 0 | 1 = 0;
   const bgDark = await PIXI.Assets.load<PIXI.Texture>("../assets/bgDark.png");
   const bgLight = await PIXI.Assets.load<PIXI.Texture>("../assets/bgLight.png");
   const BG = new PIXI.Container();
 
-  //! inefficient, just a debug;
+  //! inefficient;
   for (let Y = 0; Y < 22; Y++) {
     for (let X = 0; X < 39; X++) {
       const sprite = new PIXI.Sprite();
